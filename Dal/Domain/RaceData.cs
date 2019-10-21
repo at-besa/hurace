e8a3f668a1cfaaa5dd2a5e046cab90ff89cfac65
Sascha <github@besamail.eu>
2019-10-21 14:27:46 +0200

    fixed Array initialization

--- Dal/Domain/RaceData.cs
+++ Dal/Domain/RaceData.cs
@@ -5,9 +5,10 @@ namespace Hurace.Dal.Domain
     // DTO/Domain Object
     public class RaceData
     {
+        private const int Splittimes = 5;
         public Race Race { get; set; }
         public int SkierId { get; set; }
-        public Array[] SplitTime { get; set; }
+        public string[] SplitTime { get; set; } = new string[Splittimes];
         public bool Disqualified { get; set; }
         
         public override string ToString() =>
