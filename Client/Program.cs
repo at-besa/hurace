d61ebf65812180a5ad509d3dc65b9254ecf77ead
Schneebauer <david-schneebauer@gmx.at>
2019-11-17 20:50:04 +0100

    main refactored

--- Client/Program.cs
+++ Client/Program.cs
@@ -26,53 +26,10 @@ namespace Hurace.Client
         }
 
         public static void Main(string[] args)
-            // private static async Task Main()
         {
             IConfiguration configuration = ConfigurationUtil.GetConfiguration();
             IConnectionFactory connectionFactory =
                 DefaultConnectionFactory.FromConfiguration(configuration, "HuraceDbConnection");
-
-            //            DalTester dalTester = new DalTester(new AdoSkierDao(connectionFactory));
-            //            dalTester.TestFindById();
-            //            dalTester.TestUpdate();
-
-            //            IRaceDataDao raceDataDao = new AdoRaceDataDao(connectionFactory);
-            ////            Console.WriteLine(String.Join("\n", raceDataDao.FindAll()));
-
-            //            AdoSkierDao skierDao = new AdoSkierDao(connectionFactory);
-            //            foreach (var skier in skierDao.FindAll())
-            //            {
-            //                skierDao.Update(skier);
-            //                Console.WriteLine($"udpated {skier}");
-            //            }
-
-            //            AdoRaceDao raceDao = new AdoRaceDao(connectionFactory);
-            //            foreach (var race in raceDao.FindAll())
-            //            {
-            //                raceDao.Update(race);
-            //                Console.WriteLine($"udpated {race}");
-            //            }
-
-            //new StartListsImporter(connectionFactory).Import();
-            //new RaceDataImporter(connectionFactory).Import();
-
-            new SplittimesImporter(connectionFactory).Import();
-
-            #region Async
-
-            //PrintTitle("PersonDao.FindAllAsync", 50);
-            //await tester2.TestFindAllAsync();
-
-            //PrintTitle("PersonDao.FindByIdAsync", 50);
-            //await tester2.TestFindByIdAsync();
-
-            //PrintTitle("PersonDao.UpdateAsync", 50);
-            //await tester2.TestUpdateAsync();
-
-            //PrintTitle("TransactionsAsync", 50);
-            //await tester2.TestTransactionsAsync();
-
-            #endregion
         }
     }
 }
