15586511a683671c0af9586d3f8aaddb1787940e
Schneebauer <david-schneebauer@gmx.at>
2019-12-10 23:47:02 +0100

    fixed async bug

--- RaceControl/ViewModels/StartListViewModel.cs
+++ RaceControl/ViewModels/StartListViewModel.cs
@@ -1,46 +1,51 @@
 ﻿using Hurace.Core.Logic;
 using Hurace.Core.Logic.Model;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace RaceControl.ViewModels
 {
     public class StartListViewModel 
     {
-	    public RaceModel RunningRace { get; set; }
+	    public RaceModel RunningRace { get; set; } = new RaceModel();
+        public ICollection<RaceModel> RaceModels { get; set; } = new Collection<RaceModel>();
+
+        public ICollection<StartListModel> RunningRaceStartList { get; set; }
         private RaceLogic raceLogic;
+        private StartListLogic startListLogic = new StartListLogic();
 
-	    public StartListViewModel()
+        public StartListViewModel()
 	    {
+            init();
+        }
+
+        private async void init()
+        {
             raceLogic = new RaceLogic();
-            GetRunningRace();
+            RunningRace = await GetRunningRace();
+            RunningRaceStartList = await GetRunningRaceStartList(RunningRace);
         }
 
-        private void GetStartListToRace()
+        private async Task<ICollection<StartListModel>> GetRunningRaceStartList(RaceModel runningRace)
         {
-            throw new NotImplementedException();
+            if (runningRace == null || runningRace.Race == null)
+            {
+                return null;
+            }
+            ICollection<StartListModel> startListModels = await startListLogic.GetStartListForRaceId(runningRace.Race.Id);
+            return startListModels;
         }
 
-        private void GetSkiersToRace()
-        {
-            throw new NotImplementedException();
-        }
-
-        private async void GetRunningRace()
+        private async Task<RaceModel> GetRunningRace()
         {
             var raceModels = await raceLogic.GetRaces();
             var runningRaceModel = raceModels.
-                Where(raceModel => raceModel.Race.Status.Name.Equals("running"))
-                .FirstOrDefault();
+                FirstOrDefault(raceModel => raceModel.Race.Status.Name.Equals("running"));
 
-            if (runningRaceModel == null)
-            {
-                return;
-            }
-
-            RunningRace = new RaceModel(); 
-            RunningRace = runningRaceModel;
+            return runningRaceModel;
         }
-	}
+    }
 }
