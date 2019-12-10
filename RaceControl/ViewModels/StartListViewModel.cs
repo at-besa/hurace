e22fe7c759cb7168cdd4d6d6c83da665992cbb8e
Schneebauer <david-schneebauer@gmx.at>
2019-12-10 22:20:31 +0100

    get runningRaceModel

--- RaceControl/ViewModels/StartListViewModel.cs
+++ RaceControl/ViewModels/StartListViewModel.cs
@@ -1,35 +1,46 @@
-﻿using System;
+﻿using Hurace.Core.Logic;
+using Hurace.Core.Logic.Model;
+using System;
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Hurace.Core.Logic;
-using RaceControl.Helpers;
+using System.Linq;
 
 namespace RaceControl.ViewModels
 {
     public class StartListViewModel 
     {
-	    public ObservableCollection<RaceViewModel> Races { get; set; } = new ObservableCollection<RaceViewModel>();
-	    public RaceViewModel SelectedRace { get; set; }
-
-	    private RaceLogic logic;
+	    public RaceModel RunningRace { get; set; }
+        private RaceLogic raceLogic;
 
 	    public StartListViewModel()
 	    {
-		    ShowRaces();
-	    }
+            raceLogic = new RaceLogic();
+            GetRunningRace();
+        }
 
+        private void GetStartListToRace()
+        {
+            throw new NotImplementedException();
+        }
 
-	    private async void ShowRaces()
-	    {
-		    logic = new RaceLogic();
+        private void GetSkiersToRace()
+        {
+            throw new NotImplementedException();
+        }
 
-		    var raceModels = await logic.GetRaces();
+        private async void GetRunningRace()
+        {
+            var raceModels = await raceLogic.GetRaces();
+            var runningRaceModel = raceModels.
+                Where(raceModel => raceModel.Race.Status.Name.Equals("running"))
+                .FirstOrDefault();
 
-		    foreach (var raceModel in raceModels)
-		    {
-			    var raceViewModel = new RaceViewModel(raceModel);
-			    Races.Add(raceViewModel);
-		    }
-	    }
+            if (runningRaceModel == null)
+            {
+                return;
+            }
+
+            RunningRace = new RaceModel(); 
+            RunningRace = runningRaceModel;
+        }
 	}
 }
