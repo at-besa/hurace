4682d4c2875d64b3d95245185d5152cd0cdcde5c
Sascha <github@besamail.eu>
2019-12-15 22:43:03 +0100

    a few control for the world

--- Core.Logic/Model/RaceControlModel.cs
+++ Core.Logic/Model/RaceControlModel.cs
@@ -12,12 +12,17 @@ namespace Hurace.Core.Logic.Model
     {
         public Race Race { get; set; }
         public SkierModel SkierModel { get; set; }
-        
-        
-        public int CompareTo(RaceControlModel other) {
+        public StartListModel StartListModel { get; set; }
+
+        public int CompareTo(RaceControlModel other)
+        {
             if (ReferenceEquals(this, other)) return 0;
             if (ReferenceEquals(null, other)) return 1;
-            return Comparer<Race>.Default.Compare(Race, other.Race);
+            var raceComparison = Comparer<Race>.Default.Compare(Race, other.Race);
+            if (raceComparison != 0) return raceComparison;
+            var skierModelComparison = Comparer<SkierModel>.Default.Compare(SkierModel, other.SkierModel);
+            if (skierModelComparison != 0) return skierModelComparison;
+            return Comparer<StartListModel>.Default.Compare(StartListModel, other.StartListModel);
         }
     }
 }
