af00bdd864558b16f03f7186f441cb41394f5c3e
Sascha <github@besamail.eu>
2019-12-17 23:10:19 +0100

    wuhuuuuu

--- Core.Logic/Model/StartListMemberModel.cs
+++ Core.Logic/Model/StartListMemberModel.cs
@@ -4,9 +4,31 @@ using System.Text;
 
 namespace Hurace.Core.Logic.Model
 {
-    public class StartListMemberModel       //TODO generate IComparable<StartListMemberModel>
+    public class StartListMemberModel : IComparable<StartListMemberModel>
     {
         public SkierModel Skier { get; set; }
         public int Startposition { get; set; }
+        
+        public bool Disqualified { get; set; }
+        public bool Running { get; set; }
+        public bool Blocked { get; set; }
+        public bool Finished { get; set; }
+
+        public int CompareTo(StartListMemberModel other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            var skierComparison = Comparer<SkierModel>.Default.Compare(Skier, other.Skier);
+            if (skierComparison != 0) return skierComparison;
+            var startpositionComparison = Startposition.CompareTo(other.Startposition);
+            if (startpositionComparison != 0) return startpositionComparison;
+            var disqualifiedComparison = Disqualified.CompareTo(other.Disqualified);
+            if (disqualifiedComparison != 0) return disqualifiedComparison;
+            var runningComparison = Running.CompareTo(other.Running);
+            if (runningComparison != 0) return runningComparison;
+            var blockedComparison = Blocked.CompareTo(other.Blocked);
+            if (blockedComparison != 0) return blockedComparison;
+            return Finished.CompareTo(other.Finished);
+        }
     }
 }
--- Core.Logic/Model/StartListModel.cs
+++ Core.Logic/Model/StartListModel.cs
@@ -6,24 +6,11 @@ using Hurace.Core.Logic.Annotations;
 
 namespace Hurace.Core.Logic.Model
 {
-    public class StartListModel : IComparable<StartListModel>
+    public class StartListModel
     {
         public int raceId { get; set; }
 
         public ICollection<StartListMemberModel> StartListMembers { get; set; }
 
-        public int CompareTo(StartListModel other) {
-            if (ReferenceEquals(this, other)) return 0;
-            if (ReferenceEquals(null, other)) return 1;
-            return raceId.CompareTo(other.raceId);
-        }
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //[NotifyPropertyChangedInvocator]
-        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        //{
-        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        //}
     }
 }
