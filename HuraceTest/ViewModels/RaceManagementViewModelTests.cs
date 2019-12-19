21322b435d54074e967def3699df3c6f3df4dc0f
Sascha <github@besamail.eu>
2019-12-19 10:48:44 +0100

    renaming some

---
+++ HuraceTest/ViewModels/RaceManagementViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using RaceControl.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaceControl.ViewModels.Tests
+{
+	public class RaceManagementViewModelTests
+	{
+		[Fact()]
+		public void RaceManagementViewModelTest()
+		{
+			Assert.True(false, "This test needs an implementation");
+		}
+	}
+}
