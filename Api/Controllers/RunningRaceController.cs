4a22569e7493da2e5519b3d86e46b9df8e80b55e
David Schneebauer <david-schneebauer@gmx.at>
2019-12-27 17:33:24 +0100

    added runningrace endpoint

---
+++ Api/Controllers/RunningRaceController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hurace.Core.DAL.Ado;
+using Hurace.Core.DAL.Common;
+using Hurace.Core.DAL.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RunningRaceController : ControllerBase    
+    {
+        private readonly ILogger<RunningRaceController> _logger;
+        private AdoRaceDao _adoRaceDao;
+
+        public RunningRaceController(ILogger<RunningRaceController> logger)
+        {
+            _logger = logger;
+            var configuration = ConfigurationUtil.GetConfiguration();
+            var connectionFactory = DefaultConnectionFactory.FromConfiguration(configuration, "HuraceDbConnection");
+            _adoRaceDao = new AdoRaceDao(connectionFactory);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<RunningRaceOutDto> Get()
+        {
+            IEnumerable<Race> races = _adoRaceDao.FindAll();
+            if (races == null)
+            {
+                return NotFound();
+            }
+            var runningRace = races.FirstOrDefault(race => race.Status.Id == 2 && race.Status.Name == "running");
+            if (runningRace == null)
+            {
+                return NotFound();
+            }
+            return Ok(RunningRaceOutDto.FromRace(runningRace));
+        }
+    }
+}
---
+++ Api/RunningRaceOutDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Hurace.Core.DAL.Domain;
+
+namespace Api
+{
+    public class RunningRaceOutDto
+    {
+        public int Id { get; set; }
+        public RaceType Type { get; set; }
+        public Status Status { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Splittimes { get; set; }
+        public string Sex { get; set;  }
+
+        public static RunningRaceOutDto FromRace(Race runningRace)
+        {
+            return new RunningRaceOutDto()
+            {
+                Id = runningRace.Id,
+                Type = runningRace.Type,
+                Status =  runningRace.Status,
+                Date = runningRace.Date,
+                Name = runningRace.Name,
+                Location = runningRace.Location,
+                Splittimes = runningRace.Splittimes,
+                Sex = runningRace.Sex
+            };
+        }
+    }
+}
