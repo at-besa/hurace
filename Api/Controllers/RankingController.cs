d6bf3b91af9d947a1bd4d123f289c6f5a516b125
David Schneebauer <david-schneebauer@gmx.at>
2019-12-28 14:40:53 +0100

    WIP RankingController.cs

--- Api/Controllers/RankingController.cs
+++ Api/Controllers/RankingController.cs
@@ -1,4 +1,6 @@
-﻿using Api.Util;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Api.Util;
 using Hurace.Core.DAL.Ado;
 using Hurace.Core.DAL.Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -12,14 +14,21 @@ namespace Api.Controllers
     {
         private readonly ILogger<RankingController> _logger;
         private readonly AdoRaceDao _adoRaceDao;
+        private readonly AdoStartListDao _adoStartListDao;
+        private readonly AdoRaceDataDao _adoRaceDataDao;
+        private readonly AdoSplitTimeDao _adoSplitTimeDao;
         public RankingController(ILogger<RankingController> logger)
         {
             _logger = logger;
-            _adoRaceDao = new AdoRaceDao(ConnectionFactoryHolder.getInstace().getConnectionFactory());
+            var connectionFactory = ConnectionFactoryHolder.getInstace().getConnectionFactory();
+            _adoRaceDao = new AdoRaceDao(connectionFactory);
+            _adoStartListDao = new AdoStartListDao(connectionFactory);
+            _adoRaceDataDao = new AdoRaceDataDao(connectionFactory);
+            _adoSplitTimeDao = new AdoSplitTimeDao(connectionFactory);
         }
 
         [HttpGet]
-        public ActionResult Get(int runningRaceId)
+        public ActionResult<IList<RankingSkierOutDto>> Get(int runningRaceId)
         {
             Race runningRace = _adoRaceDao.FindById(runningRaceId);
             return Ok(runningRace.Name);
