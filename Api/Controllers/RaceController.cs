fd5fcb4cf95a3dca04a2140302597c70e05ac20e
David Schneebauer <david.schneebauer@gmx.at>
2020-01-02 21:59:43 +0100

    added RaceController.cs

---
+++ Api/Controllers/RaceController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Api.Util;
+using Hurace.Core.DAL.Ado;
+using Hurace.Core.DAL.Common;
+using Hurace.Core.DAL.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RaceController : ControllerBase
+    {
+        private readonly ILogger<RaceController> _logger;
+        private readonly AdoRaceDao _adoRaceDao;
+        public RaceController(ILogger<RaceController> logger)
+        {
+            _logger = logger;
+            IConnectionFactory connectionFactory = ConnectionFactoryHolder.getInstace().getConnectionFactory(); 
+            _adoRaceDao = new AdoRaceDao(connectionFactory);
+        }
+        [HttpGet]
+        public ActionResult<IList<RaceOutDto>> GetAll()
+        {
+            IList<RaceOutDto> raceOutDtos = new List<RaceOutDto>();
+            IEnumerable<Race> races = _adoRaceDao.FindAll();
+            if (races == null)
+            {
+                return NotFound();
+            }
+            foreach (var race in races)
+            {
+                raceOutDtos.Add(new RaceOutDto()
+                {
+                    Location = race.Location,
+                    Name = race.Name,
+                    Sex = race.Sex,
+                    TypeId = race.Type.Id,
+                    TypeName = race.Type.Type
+                });
+            }
+            return Ok(raceOutDtos);
+        }
+    }
+}
---
+++ Api/Dtos/Out/RaceOutDto.cs
@@ -0,0 +1,20 @@
+﻿using Hurace.Core.DAL.Domain;
+using System;
+using System.Linq;
+
+namespace Api
+{
+    public class RaceOutDto
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Sex { get; set; }
+        public int TypeId { get; set; }
+        public string TypeName { get; set; }
+
+        public static RaceOutDto FromRace(IGrouping<RaceType, Race> grouping)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
