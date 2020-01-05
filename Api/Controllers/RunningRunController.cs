43bccd0a1bfe34af742938a151ff7bd69e5fd3b6
David Schneebauer <david.schneebauer@gmx.at>
2020-01-05 22:37:50 +0100

    added Run Controller

---
+++ Api/Controllers/RunningRunController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Api.Util;
+using Hurace.Core.DAL.Ado;
+using Hurace.Core.DAL.Domain;
+using Hurace.Core.DAL.Importer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("RunningRace/{runningRaceId:int}/[controller]")]
+    public class RunningRunController : ControllerBase
+    {
+        private readonly ILogger<RaceController> _logger;
+        private readonly AdoRunDao _adoRunDao;
+        private readonly AdoRaceDao _adoRaceDao;
+        public RunningRunController(ILogger<RunningRunController> _logger)
+        {
+            var connectionFactory = ConnectionFactoryHolder.getInstace().getConnectionFactory();
+            _adoRunDao = new AdoRunDao(connectionFactory);
+            _adoRaceDao = new AdoRaceDao(connectionFactory);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<RunningRunOutDto> Get(int runningRaceId)
+        {
+            //new RunImporter(ConnectionFactoryHolder.getInstace().getConnectionFactory()).Import();
+            RunningRunOutDto runningRun = null;
+            Race race = _adoRaceDao.FindById(runningRaceId);
+            if (race == null)
+            {
+                return NotFound();
+            }
+            if (race.Status?.Id != 2 || race.Status?.Name != "running")
+            {
+                return NotFound();
+            }
+
+            for (int i = 0; i < race.Type?.NumberOfRuns; i++)
+            {
+                var run = _adoRunDao.FindById(race.Id, i + 1);
+                if (run.Running == true && run.Finished == false)
+                {
+                    runningRun = RunningRunOutDto.FromRun(run);
+                }
+            }
+
+            if (runningRun == null)
+            {
+                return NotFound();
+            }
+            return Ok(runningRun);
+        }
+        
+    }
+}
---
+++ Api/Dtos/Out/RunningRunOutDto.cs
@@ -0,0 +1,23 @@
+﻿using Hurace.Core.DAL.Domain;
+
+namespace Api
+{
+    public class RunningRunOutDto
+    {
+        public int RaceId { get; set; }
+        public int RunNo { get; set; }
+        public bool Running { get; set; }
+        public bool Finnished { get; set; }
+
+        public static RunningRunOutDto FromRun(Run run)
+        {
+            return new RunningRunOutDto()
+            {
+                RaceId = run.RaceId,
+                RunNo = run.RunNo,
+                Running = run.Running,
+                Finnished = run.Finished
+            };
+        }
+    }
+}
